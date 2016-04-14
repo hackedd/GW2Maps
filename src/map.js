338289131fda55900d0ef1df2ba84fa41a10fd00
Paul Hooijenga <paulhooijenga@gmail.com>
2016-04-14 21:55:17 +0200

    make stuff compatible with browserify

--- src/map.js
+++ src/map.js
@@ -1,130 +1,132 @@
-define(["leaflet", "jquery"], function (L, $) {
-    function getCoordinates(args) {
-        if (args.length === 1) {
-            return args[0];
-        }
-        if (args.length === 2) {
-            return [args[0], args[1]];
-        }
-        throw new Error("Invalid number of arguments");
+var $ = require("jquery"),
+    L = require("leaflet"),
+    GW2Map;
+
+function getCoordinates(args) {
+    if (args.length === 1) {
+        return args[0];
     }
+    if (args.length === 2) {
+        return [args[0], args[1]];
+    }
+    throw new Error("Invalid number of arguments");
+}
 
-    var GW2Map = L.Class.extend({
-        includes: L.Mixin.Events,
+GW2Map = L.Class.extend({
+    includes: L.Mixin.Events,
 
-        options: {
-            zoomClassNamePrefix: "zoom-"
-        },
+    options: {
+        zoomClassNamePrefix: "zoom-"
+    },
 
-        initialize: function (domId, continent, floor, options) {
-            var self = this;
+    initialize: function (domId, continent, floor, options) {
+        var self = this;
 
-            L.setOptions(self, options);
+        L.setOptions(self, options);
 
-            self.loadFired = false;
+        self.loadFired = false;
 
-            self.domObj = $("#" + domId);
-            if (!self.domObj.length) {
-                throw new Error("There is no element with id '" + domId + "'");
+        self.domObj = $("#" + domId);
+        if (!self.domObj.length) {
+            throw new Error("There is no element with id '" + domId + "'");
+        }
+
+        $.get("https://api.guildwars2.com/v1/continents.json", function (data) {
+            if (data.continents[continent] !== undefined) {
+                self.continent = data.continens[continent];
+                self.continent.id = continent;
+            } else {
+                $.each(data.continents, function (id) {
+                    if (this.name === continent) {
+                        self.continent = this;
+                        self.continent.id = id;
+                        return false;
+                    }
+                });
             }
 
-            $.get("https://api.guildwars2.com/v1/continents.json", function (data) {
-                if (data.continents[continent] !== undefined) {
-                    self.continent = data.continens[continent];
-                    self.continent.id = continent;
-                } else {
-                    $.each(data.continents, function (id) {
-                        if (this.name === continent) {
-                            self.continent = this;
-                            self.continent.id = id;
-                            return false;
-                        }
-                    });
-                }
+            if (self.continent === undefined) {
+                throw new Error("Unknown continent name or id '" + continent + "'");
+            }
 
-                if (self.continent === undefined) {
-                    throw new Error("Unknown continent name or id '" + continent + "'");
-                }
-
-                /* Create Leaflet map object. */
-                self.map = L.map(domId, {
-                    minZoom: self.continent.min_zoom,
-                    maxZoom: self.continent.max_zoom,
-                    crs: L.CRS.Simple,
-                    attributionControl: false
-                });
-
-                self.fire("create");
-
-                /* Retrieve floor data and create the tile layer. */
-                self.setFloor(floor);
-
-                /* Set up event handlers to add a class with the current
-                 * zoom level to the DOM element. */
-                self.map.on("zoomstart", function () { this.domObj.removeClass(this.options.zoomClassNamePrefix + this.map.getZoom()); }, self)
-                        .on("zoomend",   function () { this.domObj.addClass(this.options.zoomClassNamePrefix + this.map.getZoom()); }, self)
-                        .fire("zoomend");
+            /* Create Leaflet map object. */
+            self.map = L.map(domId, {
+                minZoom: self.continent.min_zoom,
+                maxZoom: self.continent.max_zoom,
+                crs: L.CRS.Simple,
+                attributionControl: false
             });
-        },
 
-        project: function () {
-            return this.map.project(getCoordinates(arguments), this.map.getMaxZoom());
-        },
+            self.fire("create");
 
-        unproject: function () {
-            return this.map.unproject(getCoordinates(arguments), this.map.getMaxZoom());
-        },
+            /* Retrieve floor data and create the tile layer. */
+            self.setFloor(floor);
 
-        setFloor: function (floor) {
-            var self = this, nw, se, bounds,
-                params = { continent_id: self.continent.id, floor: floor };
+            /* Set up event handlers to add a class with the current
+             * zoom level to the DOM element. */
+            self.map.on("zoomstart", function () { this.domObj.removeClass(this.options.zoomClassNamePrefix + this.map.getZoom()); }, self)
+                    .on("zoomend",   function () { this.domObj.addClass(this.options.zoomClassNamePrefix + this.map.getZoom()); }, self)
+                    .fire("zoomend");
+        });
+    },
 
-            $.get("https://api.guildwars2.com/v1/map_floor.json", params, function (data) {
-                self.floor = floor;
-                self.floorData = data;
+    project: function () {
+        return this.map.project(getCoordinates(arguments), this.map.getMaxZoom());
+    },
 
-                /* Get map bounds from the floor data. */
-                if (data.clamped_view) {
-                    nw = self.unproject(data.clamped_view[0]);
-                    se = self.unproject(data.clamped_view[1]);
-                } else {
-                    nw = self.unproject(0, 0);
-                    se = self.unproject(data.texture_dims);
-                }
+    unproject: function () {
+        return this.map.unproject(getCoordinates(arguments), this.map.getMaxZoom());
+    },
 
-                bounds = new L.LatLngBounds([nw, se]);
+    setFloor: function (floor) {
+        var self = this, nw, se, bounds,
+            params = { continent_id: self.continent.id, floor: floor };
 
-                if (self.tileLayer) {
-                    self.tileLayer.addTo(null);
-                }
+        $.get("https://api.guildwars2.com/v1/map_floor.json", params, function (data) {
+            self.floor = floor;
+            self.floorData = data;
 
-                /* Create a tile layer for the continent and floor. */
-                self.tileLayer = L.tileLayer("https://tiles.guildwars2.com/{continent}/{floor}/{z}/{x}/{y}.jpg", {
-                    minZoom: self.continent.min_zoom,
-                    maxZoom: self.continent.max_zoom,
-                    bounds: bounds,
-                    continuousWorld: true,
-                    continent: self.continent.id,
-                    floor: self.floor
-                });
-                self.tileLayer.addTo(self.map);
+            /* Get map bounds from the floor data. */
+            if (data.clamped_view) {
+                nw = self.unproject(data.clamped_view[0]);
+                se = self.unproject(data.clamped_view[1]);
+            } else {
+                nw = self.unproject(0, 0);
+                se = self.unproject(data.texture_dims);
+            }
 
-                self.map.setMaxBounds(bounds);
-                self.map.setView(bounds.getCenter(), 2);
+            bounds = new L.LatLngBounds([nw, se]);
 
-                if (!self.loadFired) {
-                    self.loadFired = true;
-                    self.fire("load");
-                }
+            if (self.tileLayer) {
+                self.tileLayer.addTo(null);
+            }
 
-                self.fire("floorchange", {continent: self.continent, floor: floor, data: data});
+            /* Create a tile layer for the continent and floor. */
+            self.tileLayer = L.tileLayer("https://tiles.guildwars2.com/{continent}/{floor}/{z}/{x}/{y}.jpg", {
+                minZoom: self.continent.min_zoom,
+                maxZoom: self.continent.max_zoom,
+                bounds: bounds,
+                continuousWorld: true,
+                continent: self.continent.id,
+                floor: self.floor
             });
-        }
-    });
+            self.tileLayer.addTo(self.map);
 
-    return {
-        init: function (domId, continent, floor) {
-            return new GW2Map(domId, continent || "Tyria", floor || 1);
-        }
-    };
+            self.map.setMaxBounds(bounds);
+            self.map.setView(bounds.getCenter(), 2);
+
+            if (!self.loadFired) {
+                self.loadFired = true;
+                self.fire("load");
+            }
+
+            self.fire("floorchange", {continent: self.continent, floor: floor, data: data});
+        });
+    }
 });
+
+module.exports ={
+    init: function (domId, continent, floor) {
+        return new GW2Map(domId, continent || "Tyria", floor || 1);
+    }
+};
--- src/poi.js
+++ src/poi.js
@@ -1,133 +1,133 @@
-define(["leaflet", "jquery"], function (L, $) {
-    var pointTypes = {
-            "landmark": "Point of Interest",
-            "skill": "Skill Point",
-            "task": "Task",
-            "vista": "Vista",
-            "waypoint": "Waypoint",
-            "unlock": "Dungeon"
-        },
-        PoiMarker,
-        icons;
+var $ = require("jquery"),
+    L = require("leaflet"),
+    pointTypes = {
+        "landmark": "Point of Interest",
+        "skill": "Skill Point",
+        "task": "Task",
+        "vista": "Vista",
+        "waypoint": "Waypoint",
+        "unlock": "Dungeon"
+    },
+    PoiMarker,
+    icons;
 
-    PoiMarker = L.Marker.extend({
-        initialize: function (poi, latlng, options) {
-            var defaultOptions;
+PoiMarker = L.Marker.extend({
+    initialize: function (poi, latlng, options) {
+        var defaultOptions;
 
-            this.poi = poi;
+        this.poi = poi;
 
-            defaultOptions = {
-                title: this.getName(),
-                icon: icons[poi.type] || L.Marker.options.icon,
-                raiseOnHover: true
-            };
+        defaultOptions = {
+            title: this.getName(),
+            icon: icons[poi.type] || L.Marker.options.icon,
+            raiseOnHover: true
+        };
 
-            L.Marker.prototype.initialize.call(this, latlng,
-                    L.extend(defaultOptions, options || {}));
-        },
+        L.Marker.prototype.initialize.call(this, latlng,
+                L.extend(defaultOptions, options || {}));
+    },
 
-        getPoi: function () {
-            return this.poi;
-        },
+    getPoi: function () {
+        return this.poi;
+    },
 
-        getName: function () {
-            return this.poi.name || pointTypes[this.poi.type];
-        },
+    getName: function () {
+        return this.poi.name || pointTypes[this.poi.type];
+    },
 
-        setIcon: function (icon) {
-            if (icon === "default" || icon === "complete") {
-                icon = icons[this.poi.type];
-            } else if (icon === "empty" || icon === "incomplete") {
-                icon = icons[this.poi.type + "-empty"];
-            }
+    setIcon: function (icon) {
+        if (icon === "default" || icon === "complete") {
+            icon = icons[this.poi.type];
+        } else if (icon === "empty" || icon === "incomplete") {
+            icon = icons[this.poi.type + "-empty"];
+        }
 
-            L.Marker.prototype.setIcon.call(this, icon);
+        L.Marker.prototype.setIcon.call(this, icon);
+    }
+});
+
+function poiMarker(poi, options) {
+    return new PoiMarker(poi, options);
+}
+
+function createIcons() {
+    function createIcon(name) {
+        icons[name] = L.divIcon({
+            iconSize: [20, 20],
+            className: "poi-icon poi-icon-" + name
+        });
+    }
+
+    icons = {};
+    $.each(pointTypes, function (key) {
+        createIcon(key);
+        createIcon(key + "-empty");
+    });
+}
+
+function addLayer(map, type) {
+    var layer = L.featureGroup();
+
+    map.on("floorchange", function (event) {
+        layer.clearLayers();
+
+        $.each(event.data.regions, function (regionId) {
+            var region = this;
+            $.each(this.maps, function (mapId) {
+                var mapData = this;
+
+                function addPoi(poi) {
+                    /* Add the map and region information to the POI, so it can be retrieved later. */
+                    poi.mapId = mapId;
+                    poi.map = mapData;
+                    poi.regionId = regionId;
+                    poi.region = region;
+                    poiMarker(poi, map.unproject(poi.coord)).addTo(layer);
+                }
+
+                if (type !== "skill" && type !== "task") {
+                    $.each(this.points_of_interest, function () {
+                        if (!type || this.type === type) {
+                            addPoi(this);
+                        }
+                    });
+                }
+
+                if (!type || type === "skill") {
+                    $.each(this.skill_challenges, function () {
+                        this.type = "skill";
+                        addPoi(this);
+                    });
+                }
+
+                if (!type || type === "task") {
+                    $.each(this.tasks, function () {
+                        this.type = "task";
+                        addPoi(this);
+                    });
+                }
+            });
+        });
+    });
+
+    map.map.on("zoomend", function () {
+        if (map.map.getZoom() >= 4) {
+            map.map.addLayer(layer);
+        } else {
+            map.map.removeLayer(layer);
         }
     });
 
-    function poiMarker(poi, options) {
-        return new PoiMarker(poi, options);
+    if (map.map.getZoom() >= 4) {
+        layer.addTo(map.map);
     }
 
-    function createIcons() {
-        function createIcon(name) {
-            icons[name] = L.divIcon({
-                iconSize: [20, 20],
-                className: "poi-icon poi-icon-" + name
-            });
-        }
+    return layer;
+}
 
-        icons = {};
-        $.each(pointTypes, function (key) {
-            createIcon(key);
-            createIcon(key + "-empty");
-        });
-    }
+createIcons();
 
-    function addLayer(map, type) {
-        var layer = L.featureGroup();
-
-        map.on("floorchange", function (event) {
-            layer.clearLayers();
-
-            $.each(event.data.regions, function (regionId) {
-                var region = this;
-                $.each(this.maps, function (mapId) {
-                    var mapData = this;
-
-                    function addPoi(poi) {
-                        /* Add the map and region information to the POI, so it can be retrieved later. */
-                        poi.mapId = mapId;
-                        poi.map = mapData;
-                        poi.regionId = regionId;
-                        poi.region = region;
-                        poiMarker(poi, map.unproject(poi.coord)).addTo(layer);
-                    }
-
-                    if (type !== "skill" && type !== "task") {
-                        $.each(this.points_of_interest, function () {
-                            if (!type || this.type === type) {
-                                addPoi(this);
-                            }
-                        });
-                    }
-
-                    if (!type || type === "skill") {
-                        $.each(this.skill_challenges, function () {
-                            this.type = "skill";
-                            addPoi(this);
-                        });
-                    }
-
-                    if (!type || type === "task") {
-                        $.each(this.tasks, function () {
-                            this.type = "task";
-                            addPoi(this);
-                        });
-                    }
-                });
-            });
-        });
-
-        map.map.on("zoomend", function () {
-            if (map.map.getZoom() >= 4) {
-                map.map.addLayer(layer);
-            } else {
-                map.map.removeLayer(layer);
-            }
-        });
-
-        if (map.map.getZoom() >= 4) {
-            layer.addTo(map.map);
-        }
-
-        return layer;
-    }
-
-    createIcons();
-
-    return {
-        poiMarker: poiMarker,
-        addLayer: addLayer
-    };
-});
+module.exports = {
+    poiMarker: poiMarker,
+    addLayer: addLayer
+};
